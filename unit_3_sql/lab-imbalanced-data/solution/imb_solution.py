01891654c6a08c3dcc75fcd7a37a93184d713e3a
¨isi.mube¨ <isi.mube@protonmail.com>
2023-03-31 17:00:58 +0200

    up2date

--- unit_3_sql/lab-imbalanced-data/solution/imb_solution.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import confusion_matrix
-import warnings
-from sklearn.metrics import cohen_kappa_score
